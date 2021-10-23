f8606c2a3e3da51139ae8b9e25bdab26d1f617a7
LuriqueProg <luiz.prog.henri@gmail.com>
2021-10-23 10:56:08 -0300

    att

--- script.js
+++ script.js
@@ -8,8 +8,28 @@ var gostei = document.querySelector('.like')
 var naogostei = document.querySelector('.unlike')
 var fechar = document.querySelector('.remove')
 var engre = document.querySelector('.config-2')
+var cat =document.querySelector('#cat')
+var nav =document.querySelector('.ul-nav')
+var catlist = document.querySelector('.ul-cat')
+var before = document.querySelector('.before')
+var after = document.querySelector('.after')
+
 //EventListiner + Function
 
+document.querySelector('#cat').addEventListener('mouseenter', menutrocar)
+function menutrocar(){
+    nav.style.display='none'
+    catlist.style.display='flex'
+    before.style.display='none'
+    after.style.display='block'
+}
+document.querySelector('.after').addEventListener('click', menuvoltar)
+function menuvoltar(){
+    nav.style.display='flex'
+    catlist.style.display='none'
+    before.style.display='block'
+    after.style.display='none'
+}
 document.querySelector('.seletores').addEventListener('click', mudarconfig)        
 function mudarconfig(){                        
     config.style.display='none'
